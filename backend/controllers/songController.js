67ff7d96ebdce811a213b92473be2eb581cfc975
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-16 17:31:16 -0400

    preventing song funciton

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -4,25 +4,24 @@ const getSongImages = require("../api/jioSong");
 
 //method to get songs
 const song = async (req, res) => {
-	let mood = "sad"
-	let songDataList = await genSongs.generateMeta(mood);
-	console.log(songDataList.authors,  songDataList.titles)
-	const titles = songDataList.titles;
-	const authors = songDataList.authors;
-	//loop through each song and author and call getSongData
+  let mood = "sad";
+  let songDataList = await genSongs.generateMeta(mood);
+  console.log(songDataList.authors, songDataList.titles);
+  const titles = songDataList.titles;
+  const authors = songDataList.authors;
+  //loop through each song and author and call getSongData
 
-	// const titles = ["Happy", "Can't Stop the Feeling!"];
-	// const authors = ["Pharrell Williams", "Justin Timberlake"];
+  // const titles = ["Happy", "Can't Stop the Feeling!"];
+  // const authors = ["Pharrell Williams", "Justin Timberlake"];
 
-	for(let i = 0; i < titles.length; i++)
-	{
-		let image = await getSongImages.getSongData(titles[i], authors[i]);
-		console.log("heres da image link *********************: " + image);
-	}
-	// let image = await getSongImages.getSongData("Rage Against the Machine", "Killing in the Name");
-	// console.log("heres da image link *********************: " + image);
+  for (let i = 0; i < titles.length; i++) {
+    let image = await getSongImages.getSongData(titles[i], authors[i]);
+    console.log("heres da image link *********************: " + image);
+  }
+  // let image = await getSongImages.getSongData("Rage Against the Machine", "Killing in the Name");
+  // console.log("heres da image link *********************: " + image);
 };
-song();
+//song();
 module.exports = {
-	song,
+  song,
 };
