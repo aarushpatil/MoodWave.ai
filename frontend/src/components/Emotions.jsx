1ed7dcc825e5b11ce0040eea40567aa6bc1439c3
Johann Chacon <89591400+johann4y@users.noreply.github.com>
2023-09-16 17:20:52 -0500

    created the emotions class and set up the foundation for the emotion page

--- frontend/src/components/Emotions.jsx
+++ frontend/src/components/Emotions.jsx
@@ -1,5 +1,36 @@
 const Emotions = () => {
-  return <div>yoo its a new thingy brah</div>;
+  return <div className = " font-Lato flex flex-col gap-10 text-lg antialiased">
+          <div className= "flex justify-between  text-white gap-20 p-5 px-6">
+            <div className=" font-semibold ">SAD</div>
+            <div className= "font-semibold">_______________</div>
+            <div>57%</div>
+
+          </div>
+          <div className= "flex justify-between text-white gap-20 p-5 px-6">
+            <div className=" font-semibold ">SAD</div>
+            <div className= "font-semibold">_______________</div>
+            <div>23%</div>
+
+          </div>
+          <div className= "flex justify-between text-white gap-20 p-5 px-6">
+            <div className=" font-semibold ">SAD</div>
+            <div className= "font-semibold">_______________</div>
+            <div>15%</div>
+
+          </div>
+          <div className= "flex justify-between text-white gap-20 p-5 px-6">
+            <div className=" font-semibold ">SAD</div>
+            <div className= "font-semibold">_______________</div>
+            <div>3%</div>
+
+          </div>
+          <div className= "flex justify-between  text-white gap-20 p-5 px-6">
+            <div className=" font-semibold ">SAD</div>
+            <div className= "font-semibold">_______________</div> 
+            <div>2%</div>
+
+          </div>
+  </div>;
 };
 
 export default Emotions;
