a72e275146b962be16d9d8cba406da8f704656ce
Adam Nguyen <Adanatonw@gmail.com>
2023-09-16 09:47:27 -0400

    working humeAI need to integrate with frontend now

--- backend/api/humeAI.js
+++ backend/api/humeAI.js
@@ -1,12 +1,34 @@
 const axios = require("axios");
-const { TIMEOUT } = require("dns");
+const sdk = require("api")("@hume/v0#4jrag2glk1nk2de");
+
 const imageToBase64 = require("image-to-base64");
 const path = require("path");
 async function startJob() {
   try {
     const imagePath = path.join(__dirname, "../temp/download.jpg");
     const jpegBase64 = await imageToBase64(imagePath);
-    const callback_URL = "https://moodwave-adam.onrender.com" + "/callback";
+
+    const model_configurations = {
+      models: {
+        face: {
+          fps_pred: 3,
+          prob_threshold: 0.99,
+          identify_faces: false,
+          min_face_size: 60,
+          save_faces: false,
+        },
+        prosody: {
+          granularity: "utterance",
+          identify_speakers: false,
+          window: { length: 4, step: 1 },
+        },
+        language: { granularity: "word", identify_speakers: false },
+        ner: { identify_speakers: false },
+      },
+      transcription: { language: null },
+      notify: false,
+    };
+    const model_config = {};
     console.log("Trying to access:", imagePath);
     const options = {
       method: "POST",
@@ -17,7 +39,7 @@ async function startJob() {
           "multipart/form-data; boundary=---011000010111000001101001",
         "X-Hume-Api-Key": "xYkGp5L5I7ATd8BvhRDKsY9FxYGUztgHA2f8GSu8DpZAX91E",
       },
-      data: `-----011000010111000001101001\r\nContent-Disposition: form-data; name="json"\r\n\r\n{"models":{"face":{"fps_pred":3,"prob_threshold":0.99,"identify_faces":false,"min_face_size":60,"save_faces":false},"prosody":{"granularity":"utterance","identify_speakers":false,"window":{"length":4,"step":1}},"language":{"granularity":"word","identify_speakers":false},"ner":{"identify_speakers":false}},"transcription":{"language":null},"notify":false,"callback_url":${callback_URL}}}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name="file"\r\n\r\ndata:image/jpeg;name=download.jpg;base64,${jpegBase64}\r\n-----011000010111000001101001--\r\n\r\n`,
+      data: `-----011000010111000001101001\r\nContent-Disposition: form-data; name="json"\r\n\r\n${model_config}\r\n-----011000010111000001101001\r\nContent-Disposition: form-data; name="file"\r\n\r\ndata:image/jpeg;name=download.jpg;base64,${jpegBase64}\r\n-----011000010111000001101001--\r\n\r\n`,
     };
     const response = await axios.request(options);
     const data = response.data.job_id;
@@ -30,9 +52,6 @@ async function startJob() {
 
 async function getPredictions(jobID) {
   try {
-    setTimeout(function () {
-      console.log("This will be printed after 3 seconds");
-    }, 5000);
     const options = {
       method: "GET",
       url: `https://api.hume.ai/v0/batch/jobs/${jobID}/predictions`,
@@ -43,7 +62,11 @@ async function getPredictions(jobID) {
     };
 
     const response = await axios.request(options);
-    const data = response.data;
+
+    const data =
+      response.data[0].results.predictions[0].models.language
+        .grouped_predictions[0].predictions[0].emotions;
+    data.sort((a, b) => b.score - a.score);
     return data;
   } catch (error) {
     console.log(error);
--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,23 +1,23 @@
 const hume = require("../api/humeAI");
-const { EventEmitter } = require("events");
-const eventEmitter = new EventEmitter();
+
 const image = async (req, res) => {
-  // if (!req.files || Object.keys(req.files).length === 0) {
-  //     return res.status(400).send("No files were uploaded.");
-  //   }
+  try {
+    const job_id = await hume.startJob();
+    console.log("Job ID:", job_id);
 
-  //   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-  //   sampleFile = req.files.sampleFile;
-  //   uploadPath = __dirname + "/backend/temp/" + sampleFile.name;
+    // Wait for the predictions to be ready
+    const emotion = await new Promise((resolve) => {
+      setTimeout(async () => {
+        const predictions = await hume.getPredictions(job_id);
+        resolve(predictions);
+      }, 5000);
+    });
 
-  //   // Use the mv() method to place the file somewhere on your server
-  //   sampleFile.mv(uploadPath, function (err) {
-  //     if (err) return res.status(500).send(err);
-  //   });
-  const job_id = await hume.startJob();
-  console.log("Job IDD", job_id);
-  const emotion = await hume.getPredictions(job_id);
-  res.json(emotion);
+    res.json(emotion);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 module.exports = {
   image,
