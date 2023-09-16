0cb1604a55566bc1eb65dea4c802570e29626245
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-15 22:52:27 -0400

    mongoDB removed not using

--- backend/app.js
+++ backend/app.js
@@ -47,7 +47,6 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`server is listening on port ${port}...`));
   } catch (error) {
     console.error(error); // It's a good practice to log the error for debugging.
