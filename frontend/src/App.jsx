37cd530aa87b1f754110a36975e08ec0dcf16f79
GabePalomino <gabrielpalomino@outlook.com>
2023-09-16 17:18:08 -0400

    Added the songs component and conditional rendering

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,32 @@ import Cam from "./components/WebCamera";
 import { useState, useEffect } from "react";
 import Emotions from "./components/Emotions";
 import axios from "axios";
+import Songs from "./components/Songs";
 function App() {
   // const [loading, setLoading] = useState(false);
   const [showEmotion, setShowEmotion] = useState(false);
+  const [showSongs, setShowSongs] = useState(false);
   const [showCam, setShowCam] = useState(true);
   const [submitImage, setSubmitImage] = useState(false);
   const [image, setImage] = useState("");
   // const [showSongs, setShowSongs] = useState(false);
   const [emotionJSON, setEmotionJSON] = useState({});
-  const submitForm = () => {
-    setShowCam(!showCam);
-    setShowEmotion(!showEmotion);
-    setSubmitImage(!submitImage);
+
+  const State1 = () => {
+    setShowCam(true);
+    setShowEmotion(false);
+    setShowSongs(false);
+  };
+
+  const State2 = () => {
+    setShowCam(false);
+    setShowEmotion(true);
+    setShowSongs(false);
+  };
+  const State3 = () => {
+    setShowCam(false);
+    setShowEmotion(false);
+    setShowSongs(true);
   };
 
   //Make a call to the back
@@ -50,34 +64,60 @@ function App() {
 
   return (
     <>
-      <div className="bg-gradient-to-r from-backgradientbot to-backgradienttop">
+      <div className="bg-gradient-to-r from-backgradientbot to-backgradienttop h-screen overflow-auto pb-9">
         <div className="flex items-center justify-center p-3 ">
           <img src={logo} alt="moodwave" className=" max-w-10 max-h-20" />
         </div>
 
         <div className="px-5 flex justify-center flex-col items-center">
-          <div className=" backdrop-blur-sm bg-white/10 rounded-3xl p-4 w-full">
+          <div className=" backdrop-blur-sm bg-white/10 rounded-3xl p-4 ">
             {showCam ? (
               <Cam
                 setEmotionJSON={setEmotionJSON}
                 image={image}
                 setImage={setImage}
               />
-            ) : (
+            ) : showEmotion ? (
               <Emotions />
+            ) : (
+              <Songs />
             )}
           </div>
 
-          <div className="pt-10 flex justify-center">
-            <button
-              type="submit"
-              id="login-button"
-              onClick={(e) => submitForm(e)}
-              className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
-            >
-              Generate
-            </button>
-          </div>
+          {showCam ? (
+            <div className="pt-10 flex justify-center">
+              <button
+                type="submit"
+                id="login-button"
+                onClick={(e) => State2(e)}
+                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
+              >
+                Scan
+              </button>
+            </div>
+          ) : showEmotion ? (
+            <div className="pt-10 flex justify-center">
+              <button
+                type="submit"
+                id="login-button"
+                onClick={(e) => State3(e)}
+                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
+              >
+                Generate
+              </button>
+            </div>
+          ) : (
+            <div className="pt-10 flex justify-center">
+              <button
+                type="submit"
+                id="login-button"
+                onClick={(e) => State1(e)}
+                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
+              >
+                Again
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
--- frontend/src/components/Songs.jsx
+++ frontend/src/components/Songs.jsx
@@ -1,20 +1,55 @@
+import drake from "../images/godsplan.jpg";
+
 const Songs = () => {
   return (
-    <div className=" font-Lato">
-      <div>
-        <img src="" />
-        <h1>Gods Plan</h1>
-        <p>Drake</p>
+    <div className=" font-Lato grid grid-rows-3 grid-cols-2 w-full gap-4 text-white">
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
       </div>
-      <div>
-        <img src="" />
-        <h1>Gods Plan</h1>
-        <p>Drake</p>
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
       </div>
-      <div>
-        <img src="" />
-        <h1>Gods Plan</h1>
-        <p>Drake</p>
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <img
+          src={drake}
+          className="w-full object-cover aspect-square rounded-xl hover:scale-105 duration-300"
+        />
+        <h1 className="text-xl">Gods Plan</h1>
+        <p className="text-sm text-gray-400">Drake</p>
       </div>
     </div>
   );
