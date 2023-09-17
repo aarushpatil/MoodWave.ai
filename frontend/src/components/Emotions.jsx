2e7e9cff1e764203ac866ea9067dc2a43f4ee0c5
GabePalomino <gabrielpalomino@outlook.com>
2023-09-16 23:38:21 -0400

    Added the loader animation

--- frontend/src/components/Emotions.jsx
+++ frontend/src/components/Emotions.jsx
@@ -1,3 +1,5 @@
+import loader from "../images/reload.png";
+
 const Emotions = ({ emotionJSON }) => {
   return (
     <div className="backdrop-blur-sm bg-white/10 rounded-3xl p-4 w-full">
@@ -5,7 +7,7 @@ const Emotions = ({ emotionJSON }) => {
         emotionJSON.map((emotion) => {
           const { name, score } = emotion;
           return (
-            <div className="grid grid-cols-3 text-white p-5 px-6 " key={name}>
+            <div className="grid grid-cols-3 text-white py-9 px-6 " key={name}>
               <div className="font-semibold justify-self-start">{name}</div>
               <div className="font-semibold justify-self-stretch">
                 <div className="bg-red-500">-</div>
@@ -17,7 +19,9 @@ const Emotions = ({ emotionJSON }) => {
           );
         })
       ) : (
-        <div className=" p-36 w-full text-center">LOADING</div>
+        <div className=" p-36 py-48 w-full text-center">
+          <img src={loader} className=" animate-spin" />
+        </div>
       )}
     </div>
   );
