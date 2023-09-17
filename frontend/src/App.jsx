b16d56f15a3b64b192db4c253a71d3a6e9f6ecd9
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-16 20:54:06 -0400

    Restructured app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,88 +5,12 @@ import { Routes, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 
 //Pages
+import Generator from "./page/Generator";
+import LandingPage from "./page/Landing";
+import AboutPage from "./page/AboutPage";
+import NotFound from "./page/NotFound";
 
-import Cam from "./components/WebCamera";
-import { useState, useEffect } from "react";
-import Emotions from "./components/Emotions";
-import axios from "axios";
-import Songs from "./components/Songs";
-import Banner from "./components/MoodWaveBanner";
 function App() {
-  // const [loading, setLoading] = useState(false);
-  const [showEmotion, setShowEmotion] = useState(false);
-  const [showSongs, setShowSongs] = useState(false);
-  const [showCam, setShowCam] = useState(true);
-  const [submitImage, setSubmitImage] = useState(false);
-  const [image, setImage] = useState("");
-  // const [showSongs, setShowSongs] = useState(false);
-  const [emotionJSON, setEmotionJSON] = useState([]);
-  const [songsJSON, setSongsJSON] = useState([]);
-  const State1 = () => {
-    setShowCam(true);
-    setShowEmotion(false);
-    setShowSongs(false);
-    setImage("");
-  };
-
-  const State2 = () => {
-    setShowCam(false);
-    setShowEmotion(true);
-    setShowSongs(false);
-    setSubmitImage(!submitImage);
-  };
-  const State3 = () => {
-    setEmotionJSON([]);
-    setShowCam(false);
-    setShowEmotion(false);
-    setShowSongs(true);
-  };
-
-  //Make a call to the back
-  useEffect(() => {
-    //Function for backend call
-    async function evaluateEmotion() {
-      if (image !== "" && submitImage) {
-        console.log("image evaluating");
-
-        const response = await axios.post(
-          "http://localhost:4000/api/v1/image/url",
-          { image: image }
-        );
-
-        const data = response.data;
-        console.log(data);
-        setEmotionJSON(data);
-        setSubmitImage(!submitImage);
-      }
-    }
-
-    //Call function
-    evaluateEmotion();
-  }, [image, setEmotionJSON, submitImage]);
-
-  useEffect(() => {
-    //Function for backend call
-    async function suggestSongs() {
-      if (emotionJSON) {
-        console.log("image evaluating");
-
-        const response = await axios.post(
-          "http://localhost:4000/api/v1/songs",
-          {
-            emotion: `${emotionJSON[0].name},${emotionJSON[0].name},${emotionJSON[0].name}`,
-          }
-        );
-
-        const data = response.data;
-        console.log(data);
-        setSongsJSON(data);
-      }
-    }
-
-    //Call function
-    suggestSongs();
-  }, [emotionJSON]);
   //trying to show different content other than the camera and capture button
   //the different content would be the data retrieved from api backend
 
@@ -98,69 +22,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/capture" element={<CapturePage />} />
-        <Route path="/emotions" element={<MoodsPage />} />
-        <Route path="/songs" element={<SongsPage />} />
+        <Route path="/generator" element={<Generator />} />
 
         {/* Extra pages */}
         <Route path="/about" element={<AboutPage />} />
-
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <div className="bg-gradient-to-r from-backgradientbot to-backgradienttop h-screen overflow-auto pb-9">
-        <Banner />
-
-        <div className="px-5 flex justify-center flex-col items-center">
-          <div className=" backdrop-blur-sm bg-white/10 rounded-3xl p-4 w-full">
-            {showCam ? (
-              <Cam
-                setEmotionJSON={setEmotionJSON}
-                image={image}
-                setImage={setImage}
-              />
-            ) : showEmotion ? (
-              <Emotions emotionJSON={emotionJSON} />
-            ) : (
-              <Songs songsJSON={songsJSON} />
-            )}
-          </div>
-
-          {showCam ? (
-            <div className="pt-10 flex justify-center">
-              <button
-                type="submit"
-                id="login-button"
-                onClick={(e) => State2(e)}
-                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
-              >
-                Scan
-              </button>
-            </div>
-          ) : showEmotion && emotionJSON ? (
-            <div className="pt-10 flex justify-center">
-              <button
-                type="submit"
-                id="login-button"
-                onClick={(e) => State3(e)}
-                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
-              >
-                Generate
-              </button>
-            </div>
-          ) : (
-            <div className="pt-10 flex justify-center">
-              <button
-                type="submit"
-                id="login-button"
-                onClick={(e) => State1(e)}
-                className=" bg-button shadow-md px-8 py-2 rounded-full text-white font-Lato text-center text-lg font-bold"
-              >
-                Again
-              </button>
-            </div>
-          )}
-        </div>
-      </div>
     </BrowserRouter>
   );
 }
