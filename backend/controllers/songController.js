34b88cd10275c039d8d1a092b873557a4f862882
aarushpatil <aarushpatil4@gmail.com>
2023-09-17 04:18:04 -0400

    songcontroller edit

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -15,42 +15,12 @@ const song = async (req, res) => {
   
 
   let images = [];
-  let playURL = [];
+  for (let i = 0; i < titles.length; i++) {
+    // Introduce a delay of 1 second before each getSongData call
+    // await delay(5000);
 
-  let titless = [];
-  let authorss = [];
-
-  let counter = 0;
-  let previousSongs = new Set();
-  const numSongsToFind = 4;
-
-  //keep generating 1 song at a time till 5 total songs are found
-  while (counter < numSongsToFind) {
-    console.log(counter);
-    //openAi's stuff
-    try {
-      let songDataList = await genSongs.generateMeta(mood, previousSongs);
-      // console.log(songDataList.authors, songDataList.titles);
-      const title = songDataList.titles[0]; //contains title
-      const author = songDataList.authors[0]; //contains author
-      previousSongs.add(title + " by " + author);
-      
-
-      //make sure I can find the song
-      let stuff = await getSongImages.getSongData(title, author);
-      let image = stuff.img;
-      //make sure I haven't already found the song and that it isn't null
-      if (image != null && !images.includes(image)) {
-        titless.push(title);
-        authorss.push(author);
-        let downloadLink = stuff.downloadUrl;
-        images.push(image);
-        counter++;
-        playURL.push(downloadLink);
-      }
-    } catch (error) {
-      // console.error("Error:", error);
-    }
+    let image = await getSongImages.getSongData(titles[i], authors[i]);
+    images.push(image);
   }
   console.log("Before: " + images);
 
@@ -58,11 +28,69 @@ const song = async (req, res) => {
   //make an array of objects now
   for(let i = 0; i < images.length; i++)
   {
-    objs.push({title: titless[i], author: authorss[i],imageUrl: images[i],playUrl: playURL[i]});
+    if(images[i] != null)
+    {
+        objs.push({title: titles[i], author: authors[i],imageUrl: images[i].img, playURL: images[i].downloadUrl});
+    }
   }
   // console.log(objs);
   res.status(200).json(objs);
 };
-song();
+
+// const song = async (req, res) => {
+//   // let mood = req.body.emotion;
+//   let mood = "happy";
+//   let images = [];
+//   let playURL = [];
+
+//   let titless = [];
+//   let authorss = [];
+
+//   let counter = 0;
+//   let previousSongs = new Set();
+//   const numSongsToFind = 4;
+
+//   //keep generating 1 song at a time till 5 total songs are found
+//   while (counter < numSongsToFind) {
+//     console.log(counter);
+//     //openAi's stuff
+//     try {
+//       let songDataList = await genSongs.generateMeta(mood, previousSongs);
+//       // console.log(songDataList.authors, songDataList.titles);
+//       const title = songDataList.titles[0]; //contains title
+//       const author = songDataList.authors[0]; //contains author
+//       previousSongs.add(title + " by " + author);
+      
+
+//       //make sure I can find the song
+//       let stuff = await getSongImages.getSongData(title, author);
+//       let image = stuff.img;
+//       //make sure I haven't already found the song and that it isn't null
+//       if (image != null && !images.includes(image)) {
+//         titless.push(title);
+//         authorss.push(author);
+//         let downloadLink = stuff.downloadUrl;
+//         images.push(image);
+//         counter++;
+//         playURL.push(downloadLink);
+//       }
+//     } catch (error) {
+//       // console.error("Error:", error);
+//     }
+//   }
+//   // console.log(images);
+//   // console.log(playURL);
+//   // console.log(titless);
+//   // console.log(authorss);
+
+  // let objs = [];
+  // //make an array of objects now
+  // for(let i = 0; i < images.length; i++)
+  // {
+  //   objs.push({title: titless[i], author: authorss[i],imageUrl: images[i],playUrl: playURL[i]});
+  // }
+//   // console.log(objs);
+//   res.status(200).json(objs);
+// };
 
 module.exports = { song };
