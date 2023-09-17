ab5c0030615fcc382e5c27355252015462f69cd7
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-17 01:59:39 -0400

    songs export fixed

--- backend/controllers/songController.js
+++ backend/controllers/songController.js
@@ -12,12 +12,12 @@ const getSongImages = require("../api/jioSong");
 //   console.log(songDataList.authors, songDataList.titles);
 //   const titles = songDataList.titles;
 //   const authors = songDataList.authors;
-  
+
 //   let images = [];
 //   for (let i = 0; i < titles.length; i++) {
 //     // Introduce a delay of 1 second before each getSongData call
 //     // await delay(5000);
-    
+
 //     let image = await getSongImages.getSongData(titles[i], authors[i]);
 //     images.push(image);
 //   }
@@ -40,42 +40,41 @@ const getSongImages = require("../api/jioSong");
 // };
 
 const song = async (req, res) => {
-	let mood = req.body.emotion;
-	let images = [];
-	let playURL = [];
-	let counter = 0;
-	let previousSongs = new Set();
-	const numSongsToFind = 4;
+  let mood = req.body.emotion;
+  let images = [];
+  let playURL = [];
+  let counter = 0;
+  let previousSongs = new Set();
+  const numSongsToFind = 4;
 
-	//keep generating 1 song at a time till 5 total songs are found
-	while(counter < numSongsToFind){
-		console.log(counter);
-		//openAi's stuff
-		try{
-			let songDataList = await genSongs.generateMeta(mood, previousSongs);
-			// console.log(songDataList.authors, songDataList.titles);
-			const title = songDataList.titles[0]; //contains title
-			const author = songDataList.authors[0]; //contains author
-			previousSongs.add(title + " by " + author);
+  //keep generating 1 song at a time till 5 total songs are found
+  while (counter < numSongsToFind) {
+    console.log(counter);
+    //openAi's stuff
+    try {
+      let songDataList = await genSongs.generateMeta(mood, previousSongs);
+      // console.log(songDataList.authors, songDataList.titles);
+      const title = songDataList.titles[0]; //contains title
+      const author = songDataList.authors[0]; //contains author
+      previousSongs.add(title + " by " + author);
 
-			//make sure I can find the song
-			let stuff = await getSongImages.getSongData(title, author);
-			let image = stuff.img;
-			//make sure I haven't already found the song and that it isn't null
-			if (image != null && !(images.includes(image)))
-			{
-				let downloadLink = stuff.downloadUrl;
-				images.push(image);
-				counter++;
-				playURL.push(downloadLink);
-			}
-		}
-		catch (error) {
-			// console.error("Error:", error);
-		}
-	}
-	console.log(images);
-	console.log(playURL);
-	res.status(200).json({images, playURL});
-}
-// module.exports = {song};
+      //make sure I can find the song
+      let stuff = await getSongImages.getSongData(title, author);
+      let image = stuff.img;
+      //make sure I haven't already found the song and that it isn't null
+      if (image != null && !images.includes(image)) {
+        let downloadLink = stuff.downloadUrl;
+        images.push(image);
+        counter++;
+        playURL.push(downloadLink);
+      }
+    } catch (error) {
+      // console.error("Error:", error);
+    }
+  }
+  console.log(images);
+  console.log(playURL);
+  res.status(200).json({ images, playURL });
+};
+
+module.exports = { song };
