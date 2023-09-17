c160f9fd230656d4de6254c5b9e097d051ee7113
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-17 06:54:49 -0400

    update to loading

--- frontend/src/components/Songs.jsx
+++ frontend/src/components/Songs.jsx
@@ -3,7 +3,7 @@ import loader from "../images/reload.png";
 const Songs = ({ songsJSON }) => {
   return (
     <>
-      {songsJSON ? (
+      {songsJSON.length > 0 ? (
         <div className="font-Lato grid grid-rows-3 grid-cols-2 w-full gap-4 text-white md:grid-rows-2 md:grid-cols-3">
           {songsJSON.map((song) => {
             const { id, author, imageUrl, playUrl, title } = song;
