d95706f873313038721e62215086a0aa5d8ac3ec
Adanato <66979547+Adanato@users.noreply.github.com>
2023-09-16 23:24:14 -0400

    fixing conditional rendering of buttons

--- frontend/src/page/Landing.jsx
+++ frontend/src/page/Landing.jsx
@@ -3,6 +3,9 @@ function Landing() {
     <main className="bg-gradient-to-r from-backgradientbot to-backgradienttop h-screen overflow-auto pb-9">
       This is the landing page. Add /generator to the end of the localhost or
       domain name
+      <div>
+        <button></button>
+      </div>
     </main>
   );
 }
